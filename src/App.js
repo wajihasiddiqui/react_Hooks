8aa6af1144248924d216bf284e261febc19e9df5
wajihasiddiqui <56477014+wajihasiddiqui@users.noreply.github.com>
2021-02-24 19:34:24 +0500

    initialcommit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
 function App() {
+  const [number, setCount] = useState(0);
+
+  function addNumber() {
+    setCount(number + 1);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>React Hooks</h1>
+      <h6>{number}</h6>
+      <button onClick={addNumber}>CLICK ME</button>
+    </>
   );
 }
 
